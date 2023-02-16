dced78ff4061a1e2adbba4d464fd6f8bf5e99e59
Sasha <gayzenband@gmail.com>
2023-02-16 14:31:16 +0300

    Финальная версия программы

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿            string[] Massiv;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            Massiv = stroka.Split(' ');
+            var result = new string[Massiv .Length];
+            var realSize = 0;
+            foreach (var value in Massiv )
+            {
+                if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+            Console.ReadKey(true);
